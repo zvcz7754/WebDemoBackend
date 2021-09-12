0582acb8ff5f7e55294628a6b50caf65c7119693
yi <yi@wavegis.com.tw>
2021-09-12 14:11:55 +0800

    9_12

--- internal/entity/library.go
+++ internal/entity/library.go
@@ -1,11 +1,15 @@
 package entity
 
 type BookDO struct {
-	ID        string //`json:"id"`
-	Name      string
-	Price     string
-	Author    string
-	Publisher string
+	ID string `gorm:"ID INT auto_increment NOT NULL primary_key:id123"`
+	// Name      string `gorm:"type:varchar NOT NULL;"`
+	// Price     string `gorm:"type:varchar;"`
+	// Author    string `gorm:"type:varchar;"`
+	// Publisher string `gorm:"type:varchar;"`
+}
+
+func (bookDO BookDO) TableName() string {
+	return "book"
 }
 
 type PublisherDO struct {
@@ -16,9 +20,9 @@ type PublisherDO struct {
 }
 
 type BookVO struct {
-	ID        string
-	Name      string
-	Price     string
-	Author    string
-	Publisher string
+	ID        string `json:"id" form:"id"` //binding:"required"
+	Name      string `json:"name" form:"name"`
+	Price     string `json:"price" form:"name"`
+	Author    string `json:"author" form:"name"`
+	Publisher string `json:"publisher" form:"publisher"`
 }
--- internal/repository/libraryRepository.go
+++ internal/repository/libraryRepository.go
@@ -1,8 +1,6 @@
 package repository //主要處理ORM
 
 import (
-	"fmt"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 
@@ -11,28 +9,38 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var dsn = "root:7754@tcp(localhost:3306)/library?charset=utf8mb4&parseTime=True&loc=Local"
-var DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+var dsn = "admin:7754@tcp(192.168.0.18:3306)/library?charset=utf8mb4&parseTime=True&loc=Local"
 
 func init() {
+	var db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
+	// db.Debug().Create(&BookDO)
+	db.AutoMigrate(&BookDO{})
 
-	var BookSlice []BookDO
-	db.Table("books").Find(&BookSlice)
-	fmt.Print(BookSlice, 1)
+	// db.Create(&BookDO{ID: "16"})
+
+	// var BookSlice []BookDO
+	// db.Table("books").Find(&BookSlice)
+	// fmt.Print(BookSlice, 1)
 }
 
-func CreateData(db *gorm.DB) {
-	db.Create(&BookDO{Name: "bb", Price: 126})
-}
+// func CreateData(db *gorm.DB) { //若已經新增則返回錯誤
+// 	db.Create(&BookDO{Name: "bb", Price: 126})
+// }
 
-func ReadData(db *gorm.DB) {
-	db.First(&Book{}, 1) // 根据整形主键查找
-	//db.First(&Book{}, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
-}
+// func ReadData(db *gorm.DB) {
+// 	db.First(&Book{}, 1) // 根据整形主键查找S
+// 	//db.First(&Book{}, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
+// }
 
-func DeleteData(db *gorm.DB) {
-	db.Delete(&Book{}, 10)
-}
+// func UpdateData() {
+// 	// Select with Struct (select zero value fields)
+// db.Model(&user).Select("Name", "Age").Updates(User{Name: "new_name", Age: 0})
+// // UPDATE users SET name='new_name', age=0 WHERE id=111;
+// }
+
+// func DeleteData(db *gorm.DB) {
+// 	db.Delete(&Book{}, 10)
+// }
